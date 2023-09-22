5685ff5f05b0ed1d3bb69c122bb0e639ab6e90e1
maubert-bc <macgregor.aubertin-young@gov.bc.ca>
2023-09-21 18:24:02 -0700

    add map feature to report

--- R/create_report.R
+++ R/create_report.R
@@ -4,12 +4,21 @@
 #' @param project_id
 #'
 #' @import glue
+#' @import sf
+#' @import dplyr
+#' @import ggplot2
+#' @import bcmaps
 #'
 #' @return
 #' @export
 #'
 #' @examples
-create_report_template <- function(survey_id, project_id, prepared_for) {
+create_report_template <- function(
+    survey_id,
+    project_id,
+    prepared_for,
+    background_text
+    ) {
   resp <- prep_survey_details(survey_id, project_id)
 
   title <- resp$surveyData$survey_details$survey_name
@@ -18,30 +27,43 @@ create_report_template <- function(survey_id, project_id, prepared_for) {
   end_date <- resp$surveyData$survey_details$end_date
 
   species <- resp$surveyData$species$focal_species_names[[1]]
-#
-#   study_area_description <- resp$surveyData$location$survey_area_name
-  prepared_for <- 'Ministry of Forests'
-#
-#   background <- 'Custom background text'
-#
-#   study_area_geometry <- create_polygon(resp$surveyData$locations[[1]]$geojson[[1]]$geometry$coordinates[[1]])
-#
-  # timestamp <-gsub(':', '-', Sys.time())
+  #
+  #   study_area_description <- resp$surveyData$location$survey_area_name
+  prepared_for <- "Ministry of Forests"
+  #
+  #   background <- 'Custom background text'
+
+  #
+  study_area_geometry <- create_polygon(resp$surveyData$locations[[1]]$geojson[[1]]$geometry$coordinates[[1]])
+
+  #
+  timestamp <- 1 # gsub(':', '-', Sys.time())
 
   file <- system.file("rmarkdown", "templates", "report", "skeleton", "skeleton.Rmd", package = "simsbc")
 
   # rmarkdown::render(file, output_file = glue("report_template_{timestamp}"))
-  rmarkdown::render(file, output_file = glue("report_template"))
+  rmarkdown::render(file, output_file = paste0("report_template_", timestamp))
   # rmarkdown::draft(file = 'report_template', template = 'rmarkdown/templates', package = 'simsbc')
 }
 
-#' #'
-#' #' @keywords internal
-#' create_polygon <- function(vertices) {
-#'   coords <- list()
-#'   for (c in 1:length(vertices)) {
-#'     print(vertices)
-#'     coords[[c]] <- c(vertices[[c]][[1]], vertices[[c]][[2]])
-#'   }
-#'   coords
-#' }
+#'
+#' @keywords internal
+create_polygon <- function(vertices) {
+
+  coords <- list()
+  for (c in 1:length(vertices)) {
+    # vertex <- vertices[[c]]
+    coords[[c]] <- c(vertices[[c]][[1]], vertices[[c]][[2]])
+    names(coords[[c]]) <- c('lon', 'lat')
+  }
+  df <- do.call(rbind.data.frame, coords)
+  names(df) <- c('lon', 'lat')
+
+  df |>
+    mutate(group = 1) |>
+    st_as_sf(coords = c("lon", "lat"),
+             crs = 4326) |>
+    group_by(group) |>
+    summarise(geometry = st_combine(geometry)) |>
+    st_cast("POLYGON")
+}
